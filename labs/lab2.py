da78b5de944b818ef9c435bab39ad1e6f1660c46
Hannes Elsen <hannes.elsen@outlook.com>
2026-04-23 17:36:01 +0200

    finished lab 2

--- labs/lab2.py
+++ labs/lab2.py
@@ -1,60 +1,90 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.sparse as sps
 
 # Copied from lab1
 
 
-def solve_FEM_Poisson_non_homogeneous(N):
-    h = 1/N
+def assemble_mass_matrix(N):
+    h = 1 / N
     x = np.linspace(0, 1, N + 1)
-    A_E = np.array([[1, -1], [-1, 1]]) / h
+    M_E = np.array([[2, 1], [1, 2]]) * h / 6
 
-    A = np.zeros((N + 1, N + 1))
-
-    for i in range(N):
-        loc_dofs = [i, i+1]
-        A[np.ix_(loc_dofs, loc_dofs)] += A_E
-
-    b = np.zeros(N + 1)
+    M = np.zeros((N + 1, N + 1))
 
     for i in range(N):
-        loc_dofs = [i, i+1]
-        b[loc_dofs] += h / 2
+        loc_dofs = [i, i + 1]
+        M[np.ix_(loc_dofs, loc_dofs)] += M_E
 
-    u = np.zeros(N + 1)
-    u[0] = 1
-
-    b -= A @ u
-
-    freedofs = np.arange(1, N + 1)
-    A_free = A[np.ix_(freedofs, freedofs)]
-    b_free = b[freedofs]
-
-    b_free[-1] += -1
-
-    u_free = np.linalg.solve(A_free, b_free)
-
-    u[freedofs] = u_free
-
-    return u, x
+    return M
 
 
-u, x = solve_FEM_Poisson_non_homogeneous(10)
+def assemble_div_matrix(N):
+    h = 1 / N
+    x = np.linspace(0, 1, N + 1)
+    B_E = np.array([-1, 1])
 
-plt.plot(x, u)
-plt.show()
+    B = np.zeros((N, N + 1))
+
+    for i in range(N):
+        loc_dofs = [i, i + 1]
+        B[i, loc_dofs] += B_E
+
+    return B
 
 
-# Mass matrix computation
-N = 10
-h = 1/N
+def assemble_SPP(N):
+    A = assemble_mass_matrix(N)
+    B = assemble_div_matrix(N)
 
-M_E = np.array([[2, 1], [1, 2]]) * h / 6
+    spp = sps.block_array([[A, B.T], [B, None]])
+    return spp
 
-M = np.zeros((N + 1, N + 1))
 
-for i in range(N):
-    loc_dofs = [i, i+1]
-    M[np.ix_(loc_dofs, loc_dofs)] += M_E
+# def aaaa(N):
 
-print(M * 6)
+#     b = np.zeros(N + 1)
+
+#     for i in range(N):
+#         loc_dofs = [i, i + 1]
+#         b[loc_dofs] += h / 2
+
+#     u = np.zeros(N + 1)
+#     u[0] = 1
+
+#     b -= M @ u
+
+#     freedofs = np.arange(1, N + 1)
+#     A_free = M[np.ix_(freedofs, freedofs)]
+#     b_free = b[freedofs]
+
+#     b_free[-1] += -1
+
+#     u_free = np.linalg.solve(A_free, b_free)
+
+#     u[freedofs] = u_free
+
+#     return u, x
+
+
+if __name__ == "__main__":
+    # Mass matrix computation
+    N = 10
+    spp = assemble_SPP(N)
+
+    plt.spy(spp)
+    plt.show()
+
+    pass
+
+    # h = 1 / N
+
+    # M_E = np.array([[2, 1], [1, 2]]) * h / 6
+
+    # M = np.zeros((N + 1, N + 1))
+
+    # for i in range(N):
+    #     loc_dofs = [i, i + 1]
+    #     M[np.ix_(loc_dofs, loc_dofs)] += M_E
+
+    # print(M * 6)

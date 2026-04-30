97c6aeb054343f3b67e4f3ff95704707c196468d
wmboon <wietseboon@gmail.com>
2026-04-30 15:50:59 +0000

    finished 1D Dirichlet MFEM

--- labs/lab2.py
+++ labs/lab2.py
@@ -5,41 +5,68 @@ import scipy.sparse as sps
 # Copied from lab1
 
 
-def assemble_mass_matrix(N):
+def grid(N: int):
     h = 1/N
     x = np.linspace(0, 1, N + 1)
+
+    return x, h
+
+
+def assemble_mass_matrix(N, h):
     M_E = np.array([[2, 1], [1, 2]]) * h / 6
 
-    M = np.zeros((N + 1, N + 1))
+    rows = np.array([], dtype=int)
+    cols = np.array([], dtype=int)
+    vals = np.array([])
 
     for i in range(N):
         loc_dofs = [i, i+1]
-        M[np.ix_(loc_dofs, loc_dofs)] += M_E
+        rows = np.append(rows, np.repeat(loc_dofs, 2))
+        cols = np.append(cols, np.tile(loc_dofs, 2))
+        vals = np.append(vals, M_E.flatten())
 
-    return M
+    return sps.csc_array((vals, (rows, cols)))
 
 
 def assemble_div_matrix(N):
-    h = 1/N
-    x = np.linspace(0, 1, N + 1)
     B_E = np.array([-1, 1])
 
-    B = np.zeros((N, N + 1))
+    rows = np.array([], dtype=int)
+    cols = np.array([], dtype=int)
+    vals = np.array([])
 
     for i in range(N):
         loc_dofs = [i, i+1]
-        B[i, loc_dofs] += B_E
+        rows = np.append(rows, np.repeat(i, 2))
+        cols = np.append(cols, loc_dofs)
+        vals = np.append(vals, B_E)
 
-    return B
+    return sps.csc_array((vals, (rows, cols)))
 
 
-def assemble_SPP(N):
-    A = assemble_mass_matrix(N)
+def assemble_SPP(N, h):
+    """ 
+    assembles the saddle point problem
+     """
+    A = assemble_mass_matrix(N, h)
     B = assemble_div_matrix(N)
 
-    spp = sps.block_array([[A, B.T], [B, None]])
+    spp = sps.block_array([[A, -B.T], [B, None]]).tocsc()
 
     return spp
+
+
+def assemble_rhs(N, x: np.array, h) -> np.array:
+    cell_centers = (x[:-1] + x[1:]) / 2
+
+    rhs_p = np.array([source(x) * h for x in cell_centers])
+    rhs_v = np.zeros(N + 1)
+
+    return np.hstack((rhs_v, rhs_p))
+
+
+def source(x: float) -> float:
+    return (2 * np.pi)**2 * np.sin(2 * np.pi * x)
 
 
 def new_func():
@@ -67,12 +94,84 @@ def new_func():
     return u, x
 
 
-if __name__ == "__main__":
-    N = 10
-    spp = assemble_SPP(N)
+def solve_problem(N):
+    x, h = grid(N)
+    spp = assemble_SPP(N, h)
+    rhs = assemble_rhs(N, x, h)
 
-    plt.spy(spp)
-    plt.show()
+    sol = sps.linalg.spsolve(spp, rhs)
+
+    u_sol = sol[:N+1]
+    p_sol = sol[N+1:]
+
+    return u_sol, p_sol
+
+
+def u_true(x):
+    return - 2 * np.pi * np.cos(2 * np.pi * x)
+
+
+def p_true(x):
+    return np.sin(2 * np.pi * x)
+
+
+def compute_error_flux(u_sol, u_true, N):
+    x, _ = grid(N)
+    u_interp = np.array([u_true(x_i) for x_i in x])
+    diff = u_sol - u_interp
+
+    M = assemble_mass_matrix(N, 1/N)
+
+    error_squared = diff @ M @ diff
+    norm_squared = u_interp @ M @ u_interp
+
+    return np.sqrt(error_squared / norm_squared)
+
+
+def compute_error_pressure(p_sol, p_true, N):
+    x, h = grid(N)
+    cell_centers = (x[:-1] + x[1:]) / 2
+
+    p_interp = np.array([p_true(x_i) for x_i in cell_centers])
+    diff = p_sol - p_interp
+
+    M = h * sps.eye_array(N)
+
+    error_squared = diff @ M @ diff
+    norm_squared = p_interp @ M @ p_interp
+
+    return np.sqrt(error_squared / norm_squared)
+
+
+def compute_rate(error, N_list):
+    return np.log(error[:-1] / error[1:]) / np.log(N_list[1:] / N_list[:-1])
+
+
+if __name__ == "__main__":
+
+    N_list = 5 * 2 ** np.arange(5)
+    error_u = np.zeros(len(N_list))
+    error_p = np.zeros(len(N_list))
+
+    for i, N in enumerate(N_list):
+        u_sol, p_sol = solve_problem(N)
+        error_u[i] = compute_error_flux(u_sol, u_true, N)
+        error_p[i] = compute_error_pressure(p_sol, p_true, N)
+
+    rates_u = compute_rate(error_u, N_list)
+    rates_p = compute_rate(error_p, N_list)
+
+    x, h = grid(N)
+    pwconstant_x = np.repeat(x, 2)[1:-1]
+    pwconstant_p = np.repeat(p_sol, 2)
+
+    plt.plot(x, u_sol)
+    plt.plot(pwconstant_x, pwconstant_p)
+    # plt.show()
+
+    # plt.spy(spp)
+    plt.savefig('plot.png', dpi=150, bbox_inches='tight')
+    plt.close()
 
     pass
 

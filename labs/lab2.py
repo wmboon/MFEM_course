4a87ad918638886d1a0d67c732fc3627fc52fed3
Philipp-1803 <philippbiesen@gmail.com>
2026-05-28 17:13:09 +0200

    last squeeze

--- labs/lab2.py
+++ labs/lab2.py
@@ -13,7 +13,7 @@ class Grid:
         self.cell_centers = (self.x[:-1] + self.x[1:]) / 2
 
 
-class MixedDarcy:
+class MixedFiniteElement:
     def assemble_mass_matrix(self, grid: Grid):
         M_E = np.array([[2, 1], [1, 2]]) * grid.h / 6
 
@@ -45,7 +45,7 @@ class MixedDarcy:
         return sps.csc_array((vals, (rows, cols)))
 
     def assemble_SPP(self, grid: Grid):
-        A = self.assemble_mass_matrix(grid)
+        A = self.assemble_A_matrix(grid)
         B = self.assemble_div_matrix(grid)
 
         spp = sps.block_array(
@@ -56,36 +56,6 @@ class MixedDarcy:
         ).tocsc()
 
         return spp
-
-    def assemble_rhs(self, grid, source):
-        rhs_p = np.array([source(x) * grid.h for x in grid.cell_centers])
-        rhs_v = np.zeros(grid.N + 1)
-
-        return np.hstack((rhs_v, rhs_p))
-
-    # def new_func():
-    #     b = np.zeros(N + 1)
-
-    #     for i in range(N):
-    #         loc_dofs = [i, i + 1]
-    #         b[loc_dofs] += h / 2
-
-    #     u = np.zeros(N + 1)
-    #     u[0] = 1
-
-    #     b -= M @ u
-
-    #     freedofs = np.arange(1, N + 1)
-    #     A_free = M[np.ix_(freedofs, freedofs)]
-    #     b_free = b[freedofs]
-
-    #     b_free[-1] += -1
-
-    #     u_free = np.linalg.solve(A_free, b_free)
-
-    #     u[freedofs] = u_free
-
-    #     return u
 
     def solve_problem(self, grid, source):
         spp = self.assemble_SPP(grid)
@@ -122,6 +92,56 @@ class MixedDarcy:
         norm_pressure_squared = p_interp @ M @ p_interp
         return np.sqrt(error_squared / norm_pressure_squared)
 
+    def assemble_stiffness_matrix(self, grid: Grid):
+        M_E = 1 / grid.h * np.array([[1, -1], [-1, 1]])
+
+        rows = np.array([], dtype=int)
+        cols = np.array([], dtype=int)
+        vals = np.array([])
+
+        for i in range(grid.N):
+            loc_dofs = [i, i + 1]
+            rows = np.append(rows, np.repeat(loc_dofs, 2))
+            cols = np.append(cols, np.tile(loc_dofs, 2))
+            vals = np.append(vals, M_E.flatten())
+
+        return sps.csc_array((vals, (rows, cols)))
+
+    def assemble_rhs(self, grid, source) -> np.array:
+        rhs_p = np.array([source(x) * grid.h for x in grid.cell_centers])
+        rhs_v = np.zeros(grid.N + 1)
+
+        return np.hstack((rhs_v, rhs_p))
+
+
+class MixedDarcy(MixedFiniteElement):
+    def assemble_A_matrix(self, grid):
+        return self.assemble_mass_matrix(grid)
+
+    # def new_func():
+    #     b = np.zeros(N + 1)
+
+    #     for i in range(N):
+    #         loc_dofs = [i, i + 1]
+    #         b[loc_dofs] += h / 2
+
+    #     u = np.zeros(N + 1)
+    #     u[0] = 1
+
+    #     b -= M @ u
+
+    #     freedofs = np.arange(1, N + 1)
+    #     A_free = M[np.ix_(freedofs, freedofs)]
+    #     b_free = b[freedofs]
+
+    #     b_free[-1] += -1
+
+    #     u_free = np.linalg.solve(A_free, b_free)
+
+    #     u[freedofs] = u_free
+
+    #     return u
+
 
 class MixeDarcyRobin(MixedDarcy):
     def assemble_mass_matrix(self, grid: Grid):
@@ -137,20 +157,8 @@ class MixeDarcyRobin(MixedDarcy):
 
 
 class Stokes(MixedDarcy):
-    def assemble_mass_matrix(self, grid: Grid):
-        M_E = 1 / grid.h * np.array([[1, -1], [-1, 1]])
-
-        rows = np.array([], dtype=int)
-        cols = np.array([], dtype=int)
-        vals = np.array([])
-
-        for i in range(grid.N):
-            loc_dofs = [i, i + 1]
-            rows = np.append(rows, np.repeat(loc_dofs, 2))
-            cols = np.append(cols, np.tile(loc_dofs, 2))
-            vals = np.append(vals, M_E.flatten())
-
-        return sps.csc_array((vals, (rows, cols)))
+    def assemble_A_matrix(self, grid):
+        return self.assemble_stiffness_matrix(grid)
 
     def solve_problem(self, grid, source):
         spp = self.assemble_SPP(grid)
@@ -175,3 +183,39 @@ class Stokes(MixedDarcy):
         p_sol = sol[grid.N + 1 :]
 
         return u_sol, p_sol
+
+    def assemble_rhs(self, grid: Grid, source) -> np.array:
+
+        f_interp = [source(x) for x in grid.x]
+        f_interp = np.array(f_interp)
+
+        M = self.assemble_mass_matrix(grid)
+
+        rhs_v = M @ f_interp
+        rhs_p = np.zeros(grid.N)
+        # np.array([source(x) * grid.h for x in grid.cell_centers])
+
+        return np.hstack((rhs_v, rhs_p))
+
+
+class Herrmann_elasticity(Stokes):
+    def __init__(self, labda):
+        self.labda = labda
+
+    def assemble_SPP(self, grid):
+
+        A = self.assemble_stiffness_matrix(grid)
+        B = self.assemble_div_matrix(grid)
+        C = self.assemble_C_matrix(grid, self.labda)
+
+        spp = sps.block_array(
+            [
+                [A, -B.T],
+                [B, C],
+            ]
+        ).tocsc()
+
+        return spp
+
+    def assemble_C_matrix(self, grid: Grid, labda):
+        return 1 / labda * grid.h * sps.eye_array(grid.N)
